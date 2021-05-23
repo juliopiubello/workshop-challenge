b5c248642002e9c9bff444b1c735cb93c8af6f7c
Breno Reis <breno.dk@gmail.com>
2021-05-23 20:08:57 -0300

    Update class -> className.

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -10,7 +10,7 @@ const Logo = AiFillTwitterCircle`
 // TODO: move login/logout button to navbar.
 function UserAction() {
     if (AddTweet.isAuthenticated) {
-        return <button type="button" class="btn btn-danger" onClick={async () => { await logout(); client.clearStore()}}>Logout</button>
+        return <button type="button" className="btn btn-danger" onClick={async () => { await logout(); client.clearStore()}}>Logout</button>
     } else {
         return <button type="button" onClick={() => loginWithRedirect()}> Login </button>
     }
@@ -18,27 +18,27 @@ function UserAction() {
 
 export default () => {
     return (
-        <header class="p-3 mb-4 bg-dark text-white">
-            <div class="container">
-                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                    <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none me-4">
+        <header className="p-3 mb-4 bg-dark text-white">
+            <div className="container">
+                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none me-4">
 
                         <IconContext.Provider value={{ color: "#1DA1F2", className: "me-2", size: "32px" }}>
                             <AiFillTwitterCircle />
                         </IconContext.Provider>
-                        <span class="logo-title">Workshop Challenge</span>
+                        <span className="logo-title">Workshop Challenge</span>
                     </a>
 
-                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+                    <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
                     </ul>
 
-                    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
                         {/* TODO: Implementar uma busca :) */}
-                        <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search" />
+                        <input type="search" className="form-control form-control-dark" placeholder="Search..." aria-label="Search" />
                     </form>
 
-                    <div class="text-end">
+                    <div className="text-end">
                         {/* <UserAction /> */}
                     </div>
                 </div>
