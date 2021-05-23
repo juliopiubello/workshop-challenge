b93a85247475b593ea43bb19af39c58271854b63
Breno Reis <breno.dk@gmail.com>
2021-05-23 18:52:40 -0300

    Adiciona o Bootstrap 5 e react-bootstrap.

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
 import Posts from './components/Posts';
 import AddTweet from './components/AddTweet';
+import Navbar from './components/Navbar'
 export default function Home() {
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <AddTweet />
-      <Posts />
-    
-    </div>
+      <div>
+        <Navbar />
+        <div className={styles.container}>
+          <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <AddTweet />
+          <Posts />
+        </div>
+      </div>
   )
 }
