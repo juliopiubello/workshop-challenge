6e6d17fd126ddf35a51991d803c1775c22c94d3f
otav-o <otaviocacadordioscanio@gmail.com>
2021-05-27 17:49:49 -0300

    Create FollowList component
    It receives an array of FollowSuggestion and displays it in SideBar

---
+++ pages/components/FollowList/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { Container, Item, Title } from "./styles";
+
+/** Displays FollowSuggestion components */
+const FollowList = ({ elements }) => {
+  return (
+    <Container>
+      <Item>
+        <Title>Who to follow</Title>
+      </Item>
+
+      {elements.map((element, index) => (
+        <Item key={index}>{element}</Item>
+      ))}
+
+      {/* Actually, displays any content */}
+      <Item>Testing...</Item>
+    </Container>
+  );
+};
+
+export default FollowList;
